1c1054159bcf542c571d701633cb94c4b50ab7e2
Naren Suresh <naren.suresh.2006@gmail.com>
2023-12-07 18:57:36 -0600

    Added validation for character inputs

--- CS128H_Final_Security-Cypher/src/main.rs
+++ CS128H_Final_Security-Cypher/src/main.rs
@@ -19,9 +19,37 @@ impl AppState {
                     _ => {}
                 }
             }
-
-            self.valid_input = None;
         }
+    }
+
+    fn validate_input(&mut self) {
+        if self.has_repeat() {
+            self.valid_input = Some("repeating character".to_string());
+        }
+    }
+
+    fn has_repeat(&mut self) -> bool {
+        let mut characters: Vec<char> = vec![];
+        for i in 0..self.plugs.len() {
+            let to_check: Option<&(char, char)> = self.plugs.get(i);
+            if !to_check.is_none() {
+                if characters.contains(&to_check.unwrap().0) {
+                    return true;
+                } else {
+                    characters.push(to_check.unwrap().0);
+                }
+    
+                if characters.contains(&to_check.unwrap().1) {
+                    return true;
+                } else {
+                    characters.push(to_check.unwrap().1);
+                }
+            } else {
+                return true;
+            }
+            
+        }
+        return false;
     }
 }
 fn main() {
